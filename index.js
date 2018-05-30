a5135412af6f531f3671a802e97b332d00c3dafa
Salim Shaikh <salim.shaikh0201@gmail.com>
2018-05-30 22:21:26 +0530

    modified to take variable any city from cmd line

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 
+// let request = require('request');
+// const argv = require('yargs').argv;
+
+// let apiKey = '137df653304d71fdb152150fa1ca0658';
+// let city = argv.c || 'pune';
+// let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+	
+// request(url, function (err, response, body) {
+//   if(err){
+//     console.log('error:', error);
+//   } else {
+//   	let weather = JSON.parse(body)
+//   	let message = 'its ${weather.main.temp} degrees in ${weather.name}!';
+//     console.log(message);
+//   }
+// });
+
+
 let request = require('request');
+const argv = require('yargs').argv;
 
 let apiKey = '137df653304d71fdb152150fa1ca0658';
-let city = 'pune';
+let city = argv.c || 'gachibowli';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
   } else {
-  	let weather = JSON.parse(body)
-  	let message = 'its ${weather.main.temp} degrees in ${weather.name}!';
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
     console.log(message);
   }
 });
-
-
-// let request = require('request');
-
-// let apiKey = '137df653304d71fdb152150fa1ca0658';
-// let city = 'gachibowli';
-// let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
-
-// request(url, function (err, response, body) {
-//   if(err){
-//     console.log('error:', error);
-//   } else {
-//     let weather = JSON.parse(body)
-//     let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-//     console.log(message);
-//   }
-// });
